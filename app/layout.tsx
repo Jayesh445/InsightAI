067bb63ccdc5eb4df4dd84f091d377265ae63add
Hasan <hasanikhan2004@gmail.com>
2025-03-27 17:38:15 +0530

    hmm

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,16 +20,9 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className="antialiased">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Toaster position="top-center" />
-          <Navbar />
+       
           {children}
-        </ThemeProvider>
+      
       </body>
     </html>
   );
