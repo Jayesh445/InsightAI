0653127889503a26a75d4e893bc74600316bac29
Jayesh Gupta <141110053+Jayesh445@users.noreply.github.com>
2025-03-28 06:06:13 +0000

    hmm

--- ai/embedding.ts
+++ ai/embedding.ts
@@ -1,22 +1,22 @@
-import { embedMany } from 'ai';
-import { google } from '@ai-sdk/google';
+// import { embedMany } from 'ai';
+// import { google } from '@ai-sdk/google';
 
-const embeddingModel = google.textEmbeddingModel("text-embedding-004");
+// const embeddingModel = google.textEmbeddingModel("text-embedding-004");
 
-const generateChunks = (input: string): string[] => {
-  return input
-    .trim()
-    .split('.')
-    .filter(i => i !== '');
-};
+// const generateChunks = (input: string): string[] => {
+//   return input
+//     .trim()
+//     .split('.')
+//     .filter(i => i !== '');
+// };
 
-export const generateEmbeddings = async (
-  value: string,
-): Promise<Array<{ embedding: number[]; content: string }>> => {
-  const chunks = generateChunks(value);
-  const { embeddings } = await embedMany({
-    model: embeddingModel,
-    values: chunks,
-  });
-  return embeddings.map((e, i) => ({ content: chunks[i], embedding: e }));
-};
+// export const generateEmbeddings = async (
+//   value: string,
+// ): Promise<Array<{ embedding: number[]; content: string }>> => {
+//   const chunks = generateChunks(value);
+//   const { embeddings } = await embedMany({
+//     model: embeddingModel,
+//     values: chunks,
+//   });
+//   return embeddings.map((e, i) => ({ content: chunks[i], embedding: e }));
+// };
